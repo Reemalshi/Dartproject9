946e042a1fc1b7a00b42dce469bf991e6ed9e814
Reemalshi <138518105+Reemalshi@users.noreply.github.com>
2023-09-08 15:35:09 +0400

    tasks
    merge Sort
    for loop and break

---
+++ dart_application_9/bin/dart_application_9.dart
@@ -0,0 +1,112 @@
+
+// first task:..........................
+// List<int> mergeSort(List<int> list) {
+//   int n = list.length;
+//   if (n < 2) {
+//     return list;
+//   }
+//   int midIndexOfList = (n / 2).floor();
+//   List<int> leftList = List<int>.from(list.sublist(0, midIndexOfList));
+//   List<int> rightList = List<int>.from(list.sublist(midIndexOfList));
+  
+//   mergeSort(leftList);
+//   mergeSort(rightList);
+//   merge(leftList, rightList, list);
+  
+//   return list;
+// }
+
+// void merge(List<int> leftList, List<int> rightList, List<int> list) {
+//   int indexLeftList = leftList.length;
+//   int indexRightList = rightList.length;
+//   int i = 0;
+//   int j = 0;
+//   int k = 0;
+  
+//   while (i < indexLeftList && j < indexRightList) {
+//     if (leftList[i] <= rightList[j]) {
+//       list[k] = leftList[i];
+//       i++;
+//     } else {
+//       list[k] = rightList[j];
+//       j++;
+//     }
+//     k++;
+//   }
+  
+//   while (i < indexLeftList) {
+//     list[k] = leftList[i];
+//     i++;
+//     k++;
+//   }
+  
+//   while (j < indexRightList) {
+//     list[k] = rightList[j];
+//     j++;
+//     k++;
+//   }
+// }
+
+// void main() {
+//   List<int> sortedList = mergeSort([2, 5, 4, 8, 9]);
+//   print(sortedList);  
+// }
+
+
+// task2 :.....................................
+
+// void main() {
+//  int? A;
+//  int? B;
+//  int? C;
+// //  int? total;
+//  int? totaltaken;
+
+//  totaltaken= 91;
+ 
+// }
+
+
+// void main() {
+//   int? A; 
+//   int? B; 
+//   int? C; 
+  
+
+//   for (A = 0; A! <= 91; A++) {
+//     B = A + 10;
+//     C = B + 8;
+    
+//     if (A + B + C == 91) {
+//       break; 
+//     }
+//   }
+  
+//   print("point of A:  $A");
+//   print("point of B:  $B");
+//   print("point of C:  $C");
+// }
+
+//tasks3:.....................................
+
+void main() {
+
+  // int? x = 6;
+
+  // int? mean6 = 28;
+
+  // int? mean7 = 27;
+
+  // int? totaly =  ((x + 1) * mean7) - (x * mean6);
+  // print("the player 7 have : $totaly points ");
+
+
+
+
+}
+
+
+
+
+
+
